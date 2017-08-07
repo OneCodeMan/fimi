4db13b2c8f3cf5c1ed17bc5c10863a4e2e3d9742
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-06 23:45:27 -0400

    git ignore

--- src/img-scraper.py
+++ src/img-scraper.py
@@ -7,6 +7,19 @@ driver = webdriver.Firefox()
 desired_url = 'https://stocksnap.io'
 driver.get(desired_url)
 
+# Directory behaviour
+dir_exists = False
+dir_version = 1
+
+while not dir_exists:
+	desired_dir = 'assets/img/stocksnap' + str(dir_version)
+
+	if os.path.exists(desired_dir):
+		dir_version += 1
+	else:
+		os.makedirs(desired_dir)
+		dir_exists = True
+
 # List of image links
 image_div = driver.find_element_by_id('main')
 image_links_list = image_div.find_elements_by_tag_name('a')
@@ -15,13 +28,18 @@ image_links_list = image_div.find_elements_by_tag_name('a')
 all_hrefs = [link.get_attribute('href') for link in image_links_list]
 image_hrefs = [image_href for image_href in all_hrefs if image_href.startswith('https://stocksnap.io/photo')]
 
-for href in image_hrefs:
+for (i, href) in enumerate(image_hrefs):
     # go to link
     driver.get(href)
 
-    # find download button
-    download_btn = driver.find_element_by_class_name('download-btn')
+    # get div
+    img_div = driver.find_element_by_class_name('img-col')
 
-    # download into folder
+    # get src
+    src = img_div.find_element_by_tag_name('img').get_attribute('src')
+
+    # download into directory
+    filename = os.path.join(desired_dir, str(i) + '.jpg')
+    urllib.request.urlretrieve(src, filename)
 
 driver.close()
