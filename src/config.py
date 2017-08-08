ab641fc2050b0e9161b4314169a8e3b9e7c45c01
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-08 19:28:14 -0400

    generates well, now just gotta make it smarter

--- src/config.py
+++ src/config.py
@@ -1,10 +1,13 @@
-font_types = ['assets/font/Slabo27px-Regular.ttf'] # add more
+import itertools
+import os
+
+font_types = os.listdir('assets/fonts') # add more
 font_sizes = [size for size in range(36, 62, 2)]
-font_colors = [(0, 0, 0), (255, 255, 255)] # add more
+font_colors = [(0, 0, 0), (255, 255, 255), (2, 57, 74), (4, 27, 21), (30, 30, 36), (255, 248, 240), (255, 255, 234), (57, 62, 65), (31, 229, 223)] # add more
 text_positions = [(0, 0), (25, 25), (100, 100), (25, 100), (200, 200), (300, 300)] # add more
 
 # Reading the quotes file
-quotes_path = 'src/assets/text/quotes0.txt'
+quotes_path = 'assets/text/quotes0.txt'
 quotes_file_open = open(quotes_path, 'r')
 quotes_file = quotes_file_open.read()
 quotes_arr = quotes_file.split('\n')[:-1]
@@ -12,3 +15,6 @@ quotes_arr = quotes_file.split('\n')[:-1]
 
 # Create permutation combos of 5
 # format: [font_type, font_size, font_color, text_position, quote]
+states = [font_types, font_sizes, font_colors, text_positions, quotes_arr]
+permutations = list(itertools.product(*states))
+print(len(permutations))
--- src/content-maker.py
+++ src/content-maker.py
@@ -4,36 +4,19 @@ from PIL import ImageDraw
 import config
 import os
 
-# Fonts
-font_path = 'assets/font/Slabo27px-Regular.ttf'
-font_size = 36
-
-# Colors
-colors = [(0, 0, 0)]
-
-# Quote
-quote = 'Be patient and tough; one day this pain will be useful to you.'
-
 # Action
 i = 0
 img_path = 'assets/img/stocksnap1/'
 img_output = 'assets/img/stocksnapoutput1/'
 
+# format: [font_type, font_size, font_color, text_position, quote]
+
 for f in os.listdir(img_path):
     if f.endswith('.jpg'):
-        img = Image.open(img_path + f)
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype(font_path, font_size)
-        draw.text((25, 100), quote, colors[0], font=font)
-        img.save(img_output + 'sample-out' + str(i) + '.jpg')
-        i += 1
-
-'''
-find a way to UUID maybe like swift
-i = 0
-for each image:
-    for each permutation -> (font type, size, color, position, quote):
-        apply permutation to image
-        save image
-        i++
-'''
+        for perm in config.permutations:
+            img = Image.open(img_path + f)
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype('assets/fonts/' + perm[0], perm[1])
+            draw.text(perm[3], perm[4], perm[2], font=font)
+            img.save(img_output + 'sample-out' + str(i) + '.jpg')
+            i += 1
