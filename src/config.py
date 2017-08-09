f18eea71da17cffbab68b8f0d13c0a43ca55bc11
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-08 20:28:36 -0400

    progress! need to fix y height though

--- src/config.py
+++ src/config.py
@@ -2,9 +2,8 @@ import itertools
 import os
 
 font_types = os.listdir('assets/fonts') # add more
-font_sizes = [size for size in range(36, 62, 2)]
-font_colors = [(0, 0, 0), (255, 255, 255), (2, 57, 74), (4, 27, 21), (30, 30, 36), (255, 248, 240), (255, 255, 234), (57, 62, 65), (31, 229, 223)] # add more
-text_positions = [(0, 0), (25, 25), (100, 100), (25, 100), (200, 200), (300, 300)] # add more
+font_sizes = [size for size in range(34, 38, 2)]
+font_colors = [(255, 255, 255), (0, 0, 0)]
 
 # Reading the quotes file
 quotes_path = 'assets/text/quotes0.txt'
@@ -14,7 +13,7 @@ quotes_arr = quotes_file.split('\n')[:-1]
 
 
 # Create permutation combos of 5
-# format: [font_type, font_size, font_color, text_position, quote]
-states = [font_types, font_sizes, font_colors, text_positions, quotes_arr]
+# format: [font_type, font_size, font_color, quote]
+states = [font_types, font_sizes, font_colors, quotes_arr]
 permutations = list(itertools.product(*states))
 print(len(permutations))
--- src/content-maker.py
+++ src/content-maker.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+import textwrap
 import config
 import os
 
@@ -9,14 +10,33 @@ i = 0
 img_path = 'assets/img/stocksnap1/'
 img_output = 'assets/img/stocksnapoutput1/'
 
-# format: [font_type, font_size, font_color, text_position, quote]
+# format: [font_type, font_size, font_color, quote]
 
 for f in os.listdir(img_path):
     if f.endswith('.jpg'):
         for perm in config.permutations:
             img = Image.open(img_path + f)
             draw = ImageDraw.Draw(img)
-            font = ImageFont.truetype('assets/fonts/' + perm[0], perm[1])
-            draw.text(perm[3], perm[4], perm[2], font=font)
+            font_type = perm[0]
+            font_size = perm[1]
+            font_color = perm[2]
+            quote = perm[3]
+
+            img_width, img_height = img.size
+
+            font = ImageFont.truetype('assets/fonts/' + font_type, font_size)
+
+            lines = textwrap.wrap(quote, width=50)
+            line_height = 0
+
+            for line in lines:
+                text_width, text_height = draw.textsize(line)
+                x_text = ((img_width - text_width) / 4) - 70
+                y_text = (img_height - text_height) / 4 + line_height
+                
+                draw.text((x_text, y_text), line, font_color, font=font)
+                line_height += 40
+
+
             img.save(img_output + 'sample-out' + str(i) + '.jpg')
             i += 1
