bb39912b68f7dd73517ee93431572a154ee363c3
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-09 06:36:59 -0400

    Simple maker in progress.

---
+++ src/simple-maker.py
@@ -0,0 +1,8 @@
+from PIL import Image, ImageFont, ImageDraw
+
+im = Image.new('RGB', (100,200), (255,0,0))
+dr = ImageDraw.Draw(im)
+
+dr.rectangle(((0,0),(10,10)), fill="black", outline = "blue")
+
+im.save("rectangle.png")
