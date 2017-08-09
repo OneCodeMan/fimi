e4087de2535d23b53369a09b2c7f937411a70296
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-09 18:40:02 -0400

    working on white and black

--- src/simple-maker.py
+++ src/simple-maker.py
@@ -1,10 +1,18 @@
 # Quotes on a black background and quotes on a white background
-
 from PIL import Image, ImageFont, ImageDraw
 
-im = Image.new('RGB', (100,200), (255,0,0))
-dr = ImageDraw.Draw(im)
+## TODO: don't make it completely black, or completely white
 
-dr.rectangle(((0,0),(10,10)), fill="black", outline = "blue")
+# White
 
-im.save("rectangle.png")
+img_white = Image.new('RGBA', (1200, 1200), 'white')
+draw = ImageDraw.Draw(img_white)
+
+img_white.save('white.png')
+
+# Black
+
+img_black = Image.new('RGBA', (1200, 1200), 'black')
+draw = ImageDraw.Draw(img_black)
+
+img_black.save('black.png')
