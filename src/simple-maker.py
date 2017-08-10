0c68d6467f812a070cd5fd6a65894a20c4a11387
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-10 06:39:15 -0400

    changed file structure and got rid of hardcoding

--- src/simple-maker.py
+++ src/simple-maker.py
@@ -1,18 +1,22 @@
 # Quotes on a black background and quotes on a white background
 from PIL import Image, ImageFont, ImageDraw
 
-## TODO: don't make it completely black, or completely white
+white_rgb = (248, 248, 248)
+white_path = 'assets/img/simplewhite1/'
+black_rgb = (0, 1, 0)
+black_path = 'assets/img/simpleblack1/'
+dim = (1200, 1200)
 
 # White
 
-img_white = Image.new('RGBA', (1200, 1200), 'white')
+img_white = Image.new('RGBA', dim, white_rgb)
 draw = ImageDraw.Draw(img_white)
 
-img_white.save('white.png')
+img_white.save(white_path + 'white.png')
 
 # Black
 
-img_black = Image.new('RGBA', (1200, 1200), 'black')
+img_black = Image.new('RGBA', dim, black_rgb)
 draw = ImageDraw.Draw(img_black)
 
-img_black.save('black.png')
+img_black.save(black_path + 'black.png')
