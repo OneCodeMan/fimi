edabc2c23a456853c317f9e43f949594f1b700b9
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-11 19:29:36 -0400

    simple maker

--- src/simple-maker.py
+++ src/simple-maker.py
@@ -10,9 +10,11 @@ black_rgb = (0, 1, 0)
 black_path = 'assets/img/simpleblack1/'
 img_width, img_height = (1200, 1200)
 dim = (img_width, img_height)
-font_size = 36
-font_type = 'NotoSans-Regular.ttf'
-font_color = (0, 0, 0)
+font_size = 40
+font_type = 'Bitter-Bold.ttf'
+black_font_color = (0, 0, 0)
+white_font_color = (255, 255, 255)
+line_break_width = 40
 
 # White
 i = 0
@@ -20,7 +22,29 @@ for quote in config.quotes_arr:
     img_white = Image.new('RGBA', dim, white_rgb)
     draw = ImageDraw.Draw(img_white)
 
-    lines = textwrap.wrap(quote, width=50)
+    lines = textwrap.wrap(quote, width=line_break_width)
+    font = ImageFont.truetype('assets/fonts/' + font_type, font_size)
+    line_height = 0
+
+    for line in lines:
+        text_width, text_height = draw.textsize(line)
+        x_text = ((img_width - text_width) / 6)
+        y_text = ((img_height) / 3) + line_height
+
+        draw.text((x_text, y_text), line, black_font_color, font=font)
+        line_height += 50
+
+    img_white.save(white_path + 'white' + str(i) + '.png')
+    i += 1
+
+
+# Black
+j = 0
+for quote in config.quotes_arr:
+    img_black = Image.new('RGBA', dim, black_rgb)
+    draw = ImageDraw.Draw(img_black)
+
+    lines = textwrap.wrap(quote, width=line_break_width)
     font = ImageFont.truetype('assets/fonts/' + font_type, font_size)
     line_height = 0
 
@@ -29,16 +53,8 @@ for quote in config.quotes_arr:
         x_text = ((img_width - text_width) / 4)
         y_text = ((img_height - text_height) / 2) + line_height
 
-        draw.text((x_text, y_text), line, font_color, font=font)
+        draw.text((x_text, y_text), line, white_font_color, font=font)
         line_height += 50
 
-    img_white.save(white_path + 'white' + str(i) + '.png')
-    i += 1
-
-# Black
-'''
-img_black = Image.new('RGBA', dim, black_rgb)
-draw = ImageDraw.Draw(img_black)
-
-img_black.save(black_path + 'black.png')
-'''
+    img_black.save(black_path + 'black' + str(j) + '.png')
+    j += 1
