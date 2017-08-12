0224086f9906a4dd8c07a4842e790d7d95e71a25
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-12 12:54:26 -0400

    content-maker is good

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ image_links = images_file.split('\n')[:-1]
 
 # Fonts
 font_types = os.listdir('assets/fonts')
-font_sizes = [size for size in range(34, 38, 2)]
+font_sizes = [size for size in range(38, 48, 2)]
 font_colors = [(255, 255, 255), (0, 0, 0)]
 
 # Reading the quotes file
@@ -18,7 +18,9 @@ quotes_file_open = open(quotes_path, 'r')
 quotes_file = quotes_file_open.read()
 quotes_arr = quotes_file.split('\n')[:-1]
 
+quote_wraps = [wrap for wrap in range(30, 60, 10)]
+
 # Create permutation combos of 5
-# format: [font_type, font_size, font_color, quote]
-states = [font_types, font_sizes, font_colors, quotes_arr]
+# format: [font_type, font_size, font_color, quote, quote_wrap]
+states = [font_types, font_sizes, font_colors, quotes_arr, quote_wraps]
 permutations = list(itertools.product(*states))
--- src/content-maker.py
+++ src/content-maker.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+from PIL import ImageFilter
 import textwrap
 import config
 import os
@@ -11,42 +12,47 @@ dir_exists = False
 dir_version = 1
 
 while not dir_exists:
-	img_path = 'assets/img/stocksnap' + str(dir_version)
+    img_path = 'assets/img/stocksnap' + str(dir_version)
     img_output = 'assets/img/stocksnapoutput' + str(dir_version)
 
-	if os.path.exists(img_path) and os.path.exists(img_output):
-		dir_version += 1
-	else:
-		os.makedirs(img_path)
+    if os.path.exists(img_output):
+    	dir_version += 1
+    else:
         os.makedirs(img_output)
-		dir_exists = True
+    	dir_exists = True
 
-# format: [font_type, font_size, font_color, quote]
+# format: [font_type, font_size, font_color, quote, quote_wrap]
 
-for f in os.listdir(img_path):
-    if f.endswith('.jpg'):
-        for perm in config.permutations:
-            img = Image.open(img_path + f)
-            draw = ImageDraw.Draw(img)
-            font_type = perm[0]
-            font_size = perm[1]
-            font_color = perm[2]
-            quote = perm[3]
+def generate_images(filter=None):
+    for f in os.listdir(img_path):
+        if f.endswith('.jpg'):
+            for perm in config.permutations:
+                img_open = Image.open(img_path + '/' + f)
+                img = img_open.filter(filter) if filter else img_open
+                draw = ImageDraw.Draw(img)
+                font_type = perm[0]
+                font_size = perm[1]
+                font_color = perm[2]
+                quote = perm[3]
+                wrap = perm[4]
 
-            img_width, img_height = img.size
+                img_width, img_height = img.size
 
-            font = ImageFont.truetype('assets/fonts/' + font_type, font_size)
+                font = ImageFont.truetype('assets/fonts/' + font_type, font_size)
 
-            lines = textwrap.wrap(quote, width=50)
-            line_height = 0
+                lines = textwrap.wrap(quote, width=wrap)
+                line_height = 0
 
-            for line in lines:
-                text_width, text_height = draw.textsize(line)
-                x_text = ((img_width - text_width) / 4) - 70
-                y_text = (img_height - text_height) / 4 + line_height
+                for line in lines:
+                    text_width, text_height = draw.textsize(line)
+                    x_text = ((img_width - text_width) / 4) - 70
+                    y_text = (img_height - text_height) / 4 + line_height
 
-                draw.text((x_text, y_text), line, font_color, font=font)
-                line_height += 40
+                    draw.text((x_text, y_text), line, font_color, font=font)
+                    line_height += 40
 
 
-            img.save(img_output + 'sample-out' + str(uuid.uuid4()) + '.jpg')
+                img.save(img_output + '/' + str(uuid.uuid4()) + '.jpg')
+
+generate_images(ImageFilter.BLUR)
+generate_images()
